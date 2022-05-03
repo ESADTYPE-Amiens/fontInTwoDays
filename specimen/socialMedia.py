a99ebd38827ca0592823ad8a0a3fba85086b546a
viktorzumegen <viktor.zumegen@gmail.com>
2022-05-03 10:35:49 +0200

    make social media workable in the cloud

--- specimen/socialMedia.py
+++ specimen/socialMedia.py
@@ -1,32 +1,47 @@
-letters = "abcdefghijklmnopqrstuvwxyz".upper()
+ letters = "abcdefghijklmnopqrstuvwxyz".upper()
 s = 100
 amount = len(letters)
 
-path = '/Users/frederik/Dropbox/lectures/Amiens/Amiens20220502/fontInTwoDays/localFiles/NewestFont-TheRegular.otf'
+import os
+
+import drawBot
+
+root = "binaryFiles"
+
+
+drawBot.newDrawing()
+
+for fileName in os.listdir(root):
+    if not fileName.endswith(".otf")
+        continue
+
+    drawBot.fontPath = os.path.join(root, fileName)
+
 
 for i, letter in enumerate(letters):
     factor1 = i / amount
     
-    newPage(s*2.5, s*2.5)
-    font(path)
+    drawBot.newPage(s*2.5, s*2.5)
+    drawBot.font(path)
 
-    with savedState():
-        fill(1)
-        rect(0, 0, width(), height())
+    with drawBot.savedState():
+        drawBot.fill(1)
+        drawBot.rect(0, 0, drawBot.width(), drawBot.height())
           
-    with savedState():
-        fontSize(s)
-        text(letter, (width() / 2, height() / 2 - fontXHeight()/2), align="center")
+    with drawBot.savedState():
+        drawBot.fontSize(s)
+        drawBot.text(letter, (drawBot.width() / 2, drawBot.height() / 2 - drawBot.fontXHeight()/2), align="center")
 
-    for c in range(amount):
+    for c in drawBot.range(amount):
         factor2 = c / amount 
         letter = letters[c]
     
-        with savedState():
-            translate(width() / 2, height() / 2)                
-            rotate(360 * factor2 + 360 * factor1) 
-            translate(s, 0)
-            rotate(-90)
-            text(letter, (0, 0))
+        with drawBot.savedState():
+            drawBot.translate(drawBot.width() / 2, drawBot.height() / 2)                
+            drawBot.rotate(360 * factor2 + 360 * factor1) 
+            drawBot.translate(s, 0)
+            drawBot.rotate(-90)
+            drawBot.text(letter, (0, 0))
  
-saveImage("socialMedia.gif")
+drawBot.saveImage("socialMedia.gif")
+drawBot.endDrawing()
